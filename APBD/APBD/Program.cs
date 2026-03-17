883e6f3d89e445ee4794c54324fbc51229a281bd
Olaf Jaworski <s33536@pjwstk.edu.pl>
2026-03-17 21:30:04 +0100

    jakas zmiana do konfliktu

--- APBD/APBD/Program.cs
+++ APBD/APBD/Program.cs
@@ -1,7 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 
-Console.WriteLine("Hello, Second branch2!");
+Console.WriteLine("Jakas zmiana1");
 Console.WriteLine("Hello, Third branch!");
 
 Console.WriteLine("Hello, cos rebase1!");
