f7c052f5a5225a72442fc37b8cf75972dba55657
Olaf Jaworski <s33536@pjwstk.edu.pl>
2026-03-17 21:30:59 +0100

    zmiana do konfliktu na mainie

--- APBD/APBD/Program.cs
+++ APBD/APBD/Program.cs
@@ -1,7 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 
-Console.WriteLine("Hello, Second branch2!");
+Console.WriteLine("tutaj inna zmiana sdasdasdsad");
 Console.WriteLine("Hello, Third branch!");
 
 Console.WriteLine("Hello, cos rebase1!");
